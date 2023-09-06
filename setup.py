766a6e4b88f5f0b4cd6ed7e5bf6d1faf60807951
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-09-06 22:55:37 +0300

    init setup def

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
 """setup file"""
+from flask import Flask
+from app import app
+
+
+def setup_database(**kwargs):
+    """setup database"""
+    pass
+
+
+def setup(app: Flask, **kwargs) -> None:
+    """setup app"""
+    setup_database(**kwargs)
