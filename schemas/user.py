0205111d6014150a952b5303684a1acc492a70e6
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-09-08 17:06:17 +0300

    add user pydantic schema

--- schemas/user.py
+++ schemas/user.py
@@ -1,1 +1,10 @@
 """User schema file"""
+from pydantic import BaseModel, ConfigDict
+
+
+class UserSchema(BaseModel):
+    """User pydantic schema"""
+
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    handler: str
