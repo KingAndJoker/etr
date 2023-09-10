8ec567c3317f9629c002ecf647d624577c367d3f
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-09-10 17:50:47 +0300

    add app

--- codeforces_2BIWY/__init__.py
+++ codeforces_2BIWY/__init__.py
@@ -1,7 +1,14 @@
 """run file"""
 from flask import Flask
+from sqlalchemy import create_engine
+from .models.base import Base
 
 
 app = Flask(__name__)
+
+DATABASE_URL = "sqlite:///users.db"
+echo = True
+engine = create_engine(DATABASE_URL, echo=echo)
+Base.metadata.create_all(engine)
 
 import codeforces_2BIWY.views
