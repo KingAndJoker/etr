1e4fcca32acb98ba0f481ef618cca486151322fd
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-09-10 22:08:07 +0300

    rewrote to func that returns app

--- codeforces_2BIWY/__init__.py
+++ codeforces_2BIWY/__init__.py
@@ -1,14 +1,39 @@
 """run file"""
+import os
+
 from flask import Flask
 from sqlalchemy import create_engine
 from .models.base import Base
 
 
-app = Flask(__name__)
+def create_app() -> Flask:
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__, instance_relative_config=True)
+    
+    app.config.from_mapping(
+        # a default secret that should be overridden by instance config
+        SECRET_KEY="dev",
 
-DATABASE_URL = "sqlite:///users.db"
-echo = True
-engine = create_engine(DATABASE_URL, echo=echo)
-Base.metadata.create_all(engine)
+    )
+    # app.config.from_file("pyproject.toml")
 
-import codeforces_2BIWY.views
+    # register the database commands
+    from codeforces_2BIWY import db
+
+    # Base.metadata.create_all(app.config["DATABASE_ENGINE"])
+    db.init_db(app)
+
+    # apply the blueprints to the app
+    from codeforces_2BIWY import views
+
+    # app.register_blueprint(views.bp)
+    app.add_url_rule("/user/new", view_func=views.NewUser.as_view("new-user"))
+    app.add_url_rule("/user", view_func=views.UserView.as_view("user"))
+    # app.register_blueprint(blog.bp)
+
+    # make url_for('index') == url_for('blog.index')
+    # in another app, you might define a separate main index here with
+    # app.route, while giving the blog blueprint a url_prefix, but for
+    # the tutorial the blog will be the main index
+
+    return app
