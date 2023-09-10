179a87ac238eb8a6bc62a3ee06289536461e5299
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-09-10 22:08:58 +0300

    add get_db() init_db()

--- codeforces_2BIWY/db.py
+++ codeforces_2BIWY/db.py
@@ -1,1 +1,26 @@
 """db utils"""
+from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from codeforces_2BIWY.models.base import Base
+from codeforces_2BIWY.models.User import User
+
+
+DATABASE_URL = "sqlite:///users.db"
+ECHO = True
+engine = None
+
+
+def init_db(app: Flask) -> None:
+    engine = create_engine(DATABASE_URL, echo=ECHO)
+    User.metadata.create_all(engine)
+
+
+def get_db() -> Session:
+    global engine
+    if engine:
+        return Session(engine)
+    else:
+        engine = create_engine(DATABASE_URL, echo=ECHO)
+        return Session(engine)
