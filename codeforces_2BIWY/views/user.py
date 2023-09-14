37912ce63530f4026f18f37f15ebf05d362fe394
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-09-14 11:47:37 +0300

    move views to /codeforces_2BIWY/views/...

--- codeforces_2BIWY/views/user.py
+++ codeforces_2BIWY/views/user.py
@@ -1,33 +1,36 @@
 """views file"""
 import requests
 from sqlalchemy.orm import Session
-from flask import render_template, redirect, request
-from flask.views import MethodView
+from flask import render_template, redirect, request, Blueprint
 
 from codeforces_2BIWY.models.User import User
 from codeforces_2BIWY.schemas.user import UserSchema
 from codeforces_2BIWY.db import get_db
 
 
-def index():
-    return render_template("index.html")
+bp = Blueprint("user", __name__, url_prefix="/user")
 
 
-class NewUser(MethodView):
-    def get(self):
+@bp.route("/new", methods=["GET", "POST"])
+def new_user():
+    if request.method == "GET":
         return render_template("new.html")
-
-    def post(self):
+    elif request.method == "POST":
         handler = request.form.get("handler")
-        session: Session = get_db()
-        session.add(User(handler=handler))
-        session.commit()
+        response = requests.get(
+            f"https://codeforces.com/api/user.info?handles={handler}"
+        )
+        response_json = response.json()
+        if response_json['status'] == "OK":
+            with get_db() as session:
+                session.add(User(handler=handler))
+                session.commit()
         return redirect("/")
 
 
-class UserView(MethodView):
-    def get(self):
-        session: Session = get_db()
+@bp.route("/")
+def get_users():
+    with get_db() as session:
         users = session.query(User).all()
         users = [UserSchema.model_validate(user) for user in users]
         for i, user in enumerate(users):
@@ -37,4 +40,4 @@ class UserView(MethodView):
             response_json = response.json()
             if response_json["status"] == "OK":
                 users[i] = user.model_copy(update=response_json["result"][0])
-        return render_template("users.html", users=users)
+    return render_template("users.html", users=users)
