2ba24d19549eb3908b2901a692d35bcdd54d0277
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-09-14 13:19:06 +0300

    add /status/ handler

--- codeforces_2BIWY/views/contest_status.py
+++ codeforces_2BIWY/views/contest_status.py
@@ -1,1 +1,10 @@
 """contest status handler"""
+from flask import Blueprint, render_template
+
+
+bp = Blueprint("contest_status", __name__, url_prefix="/status")
+
+
+@bp.route("/")
+def status():
+    return render_template("status.html")
