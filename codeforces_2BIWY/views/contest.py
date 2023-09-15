5157bca0b62f7e4398b8ea8ca33f8d3f081a9d2a
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-09-15 13:37:03 +0300

    add /contest/new/

--- codeforces_2BIWY/views/contest.py
+++ codeforces_2BIWY/views/contest.py
@@ -1,7 +1,10 @@
 """contest views"""
+import requests
 from flask import (
     Blueprint,
-    render_template
+    render_template,
+    request,
+    redirect
 )
 
 from codeforces_2BIWY.db import get_db
@@ -23,3 +26,25 @@ def get_contests():
         ]
 
     return render_template("contests.html", contests=contests)
+
+
+@bp.route("/new", methods=["GET", "POST"])
+def new_contest():
+    if request.method == "GET":
+        return render_template("new_contest.html")
+
+    elif request.method == "POST":
+        contest_id = request.form.get("contest-id")
+        response = requests.get(
+            f"https://codeforces.com/api/contest.standings?"
+            f"contestId={contest_id}"
+            f"&count=1"
+        )
+        response_json = response.json()
+
+        if response_json['status'] == "OK":
+            with get_db() as session:
+                contest = Contest(**response_json["result"]["contest"])
+                session.add(contest)
+                session.commit()
+        return redirect("/")
