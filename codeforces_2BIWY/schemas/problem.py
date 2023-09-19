832d15fd683e770e58dcfbc74cb9a3d5cdbd74ed
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-09-19 19:39:16 +0300

    add | None = None

--- codeforces_2BIWY/schemas/problem.py
+++ codeforces_2BIWY/schemas/problem.py
@@ -6,12 +6,12 @@ class ProblemSchema(BaseModel):
     """Problem schema"""
     model_config = ConfigDict(from_attributes=True)
 
-    id: int 
-    contestId: int
-    problemsetName: str
-    index: str
-    name: str
-    type: str
+    id: int | None = None
+    contestId: int | None = None
+    problemsetName: str  | None = None
+    index: str | None = None
+    name: str | None = None
+    type: str | None = None
     points: float | None = None
     rating: int | None = None
-    tags: list[str] | None = None
+    # tags: list[str] | None = None
