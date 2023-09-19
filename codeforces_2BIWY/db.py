9cae107a529981c99485ba64cc45fce286eddcc0
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-09-19 19:29:14 +0300

    add to init_db Team and teams_users table

--- codeforces_2BIWY/db.py
+++ codeforces_2BIWY/db.py
@@ -8,6 +8,7 @@ from codeforces_2BIWY.models.user import User
 from codeforces_2BIWY.models.contest import Contest
 from codeforces_2BIWY.models.submission import Submission
 from codeforces_2BIWY.models.problem import Problem
+from codeforces_2BIWY.models.team import Team, teams_users
 
 
 DATABASE_URL = "sqlite:///users.db"
