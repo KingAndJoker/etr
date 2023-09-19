0468214a14eab54476efd3b52ebcc91d5dac1a15
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-09-19 19:48:11 +0300

    update author field

--- codeforces_2BIWY/schemas/submission.py
+++ codeforces_2BIWY/schemas/submission.py
@@ -1,26 +1,25 @@
 """Submission schema file"""
 from pydantic import BaseModel, ConfigDict
 
+from codeforces_2BIWY.schemas.problem import ProblemSchema
+from codeforces_2BIWY.schemas.user import UserSchema
+from codeforces_2BIWY.schemas.team import TeamSchema
+
 
 class SubmissionSchema(BaseModel):
     """
     Submission pydantic schema
-    without field:
-      author
-      testset
-      partial problem
-
     https://codeforces.com/apiHelp/objects#Submission
     """
 
     model_config = ConfigDict(from_attributes=True)
 
-    id: int
+    id: int | None = None
     contestId: int | None = None
     creationTimeSeconds: int | None = None
     relativeTimeSeconds: int | None = None
-    problemName: str | None = None
-    problemIndex: str | None = None
+    problem: ProblemSchema | None = None
+    author: UserSchema | TeamSchema | None = None
     programmingLanguage: str | None = None
     verdict: str | None = None
     passedTestCount: int | None = None
