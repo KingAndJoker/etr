98e6182b430c6efc743ec7c0d63d436bcf302157
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-09-24 17:06:01 +0300

    update field type User model

--- codeforces_2BIWY/schemas/user.py
+++ codeforces_2BIWY/schemas/user.py
@@ -1,28 +1,38 @@
 """User schema file"""
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class UserSchema(BaseModel):
     """User pydantic schema"""
 
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(
+        from_attributes=True,
+        populate_by_name=True
+    )
+
     id: int | None = None
     handle: str
     email: str | None = None
-    vkId: str | None = None
-    openId: str | None = None
-    firstName: str | None = None
-    lastName: str | None = None
+    vk_id: str | None = Field(default=None, alias="vkId")
+    open_id: str | None = Field(default=None, alias="openId")
+    first_name: str | None = Field(default=None, alias="firstName")
+    last_name: str | None = Field(default=None, alias="lastName")
     country: str | None = None
     city: str | None = None
     organization: str | None = None
     contribution: int | None = None
     rank: str | None = None
     rating: int | None = None
-    maxRank: str | None = None
-    maxRating: int | None = None
-    lastOnlineTimeSeconds: int | None = None
-    registrationTimeSeconds: int | None = None
-    friendOfCount: int | None = None
+    max_rank: str | None = Field(default=None, alias="maxRank")
+    max_rating: int | None = Field(default=None, alias="maxRating")
+    last_online_time_seconds: int | None = Field(
+        default=None,
+        alias="lastOnlineTimeSeconds"
+    )
+    registration_time_seconds: int | None = Field(
+        default=None,
+        alias="registrationTimeSeconds"
+    )
+    friend_of_count: int | None = Field(default=None, alias="friendOfCount")
     avatar: str | None = None
-    titlePhoto: str | None = None
+    title_photo: str | None = Field(default=None, alias="titlePhoto")
