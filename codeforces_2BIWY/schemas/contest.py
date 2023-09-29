c188c9420d57c50213309eb7680845ada091d7ac
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-09-29 21:12:46 +0300

    refactor Contest

--- codeforces_2BIWY/schemas/contest.py
+++ codeforces_2BIWY/schemas/contest.py
@@ -1,25 +1,34 @@
 """Contest schema"""
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class ContestSchema(BaseModel):
     """Contest schema"""
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(
+        from_attributes=True,
+        populate_by_name=True
+    )
 
-    id: int
-    name: str
+    id: int = Field(alias="contestId")
+    name: str = Field(alias="contestName")
     type: str | None = None
     phase: str | None = None
     frozen: bool | None = None
-    durationSeconds: int | None = None
-    startTimeSeconds: int | None = None
-    relativeTimeSeconds: int | None = None
-    preparedBy: str | None = None
-    websiteUrl: str | None = None
+    duration_seconds: int | None = Field(default=None, alias="durationSeconds")
+    start_time_seconds: int | None = Field(
+        default=None,
+        alias="startTimeSeconds"
+    )
+    relative_time_seconds: int | None = Field(
+        default=None,
+        alias="relativeTimeSeconds"
+    )
+    prepared_by: str | None = Field(default=None, alias="preparedBy")
+    website_url: str | None = Field(default=None, alias="websiteUrl")
     description: str | None = None
     difficulty: int | None = None
     kind: str | None = None
-    icpcRegion: str | None = None
+    icpc_region: str | None = Field(default=None, alias="icpcRegion")
     country: str | None = None
     city: str | None = None
     season: str | None = None
