c30b37b78952e73546215d5efeef89a3557a11dc
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-09-30 20:54:39 +0300

    add problem_id, author_id, team_id fields to Submission model

--- codeforces_2BIWY/models/submission.py
+++ codeforces_2BIWY/models/submission.py
@@ -1,5 +1,5 @@
 """submission model"""
-from sqlalchemy import ForeignKey, Column
+from sqlalchemy import ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from codeforces_2BIWY.models.base import Base
@@ -20,11 +20,14 @@ class Submission(Base):
     contest_id: Mapped[int] = mapped_column(nullable=True)
     creation_time_seconds: Mapped[int] = mapped_column()
     relative_time_seconds: Mapped[int] = mapped_column()
+    problem_id: Mapped[int] = mapped_column(Integer, ForeignKey("problems.id"))
     problem: Mapped[Problem] = relationship(
         "Problem",
-        back_populates="submissions"
+        back_populates="submissions",
     )
+    author_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
     author: Mapped[User] = relationship("User", back_populates="submissions")
+    team_id: Mapped[Team] = mapped_column(Integer, ForeignKey("teams.id"))
     team: Mapped[Team] = relationship("Team", back_populates="submissions")
     programming_language: Mapped[str] = mapped_column()
     verdict: Mapped[str] = mapped_column(nullable=True)
