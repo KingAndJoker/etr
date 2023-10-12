fe6e16a7bbb5f623fe4a65b88aec56e9a9131d82
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-10-12 21:58:21 +0300

    update fields Team

--- etr/schemas/team.py
+++ etr/schemas/team.py
@@ -1,12 +1,15 @@
 """Team schema file"""
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class TeamSchema(BaseModel):
     """Team pydantic schema"""
 
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(
+        from_attributes=True,
+        populate_by_name=True
+    )
 
-    id: int
-    teamName: str
+    id: int = Field(alias="teamId")
+    team_name: str = Field(alias="teamName")
     users: list["UserSchema"]
