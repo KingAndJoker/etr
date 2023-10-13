b9daecbfaefbddea04e91ae2c75667993c39ae5a
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-10-13 21:59:22 +0300

    add /submission/<contest_id> handler

--- etr/rpc.py
+++ etr/rpc.py
@@ -6,8 +6,10 @@ from flask import (
 from sqlalchemy import inspect
 
 from etr.db import get_db
-from etr.utils.codeforces_utils import get_contest
+from etr.utils.codeforces_utils import get_contest, get_submission
 from etr.models.contest import Contest
+from etr.models.submission import Submission
+from etr.models.user import User
 
 
 # TODO: https://safjan.com/guide-building-python-rpc-server-using-flask/
@@ -21,7 +23,8 @@ def update_contest_info(contest_id: int):
         return {"status": "failed"}
 
     inst = inspect(Contest)
-    attr_contest: set[str] = {c_attr.key for c_attr in inst.mapper.column_attrs}
+    attr_contest: set[str] = {
+        c_attr.key for c_attr in inst.mapper.column_attrs}
 
     with get_db() as session:
         contest_dict = contest.model_dump(include=attr_contest)
@@ -36,6 +39,33 @@ def update_contest_info(contest_id: int):
                 setattr(contest_db, field, value)
 
         session.add(contest_db)
+        session.commit()
+
+    return {"status": "ok"}
+
+
+@bp.get("/submission/<contest_id>")
+def update_submission_info(contest_id):
+    with get_db() as session:
+        users = session.query(User).filter(User.watch).all()
+
+    submissions = list()
+    for user in users:
+        submissions = get_submission(contest_id, handle=user.handle)
+
+    if submissions is None:
+        return {"status": "failed"}
+
+    with get_db() as session:
+        for i, submission in enumerate(submissions):
+            sub = session.query(Submission).filter(
+                Submission.id == submission.id and Submission.contest_id == submission.contest_id
+            ).one_or_none()
+
+            if sub is None:
+                sub = Submission(**submission.model_dump())
+            
+            session.add(sub)
         session.commit()
 
     return {"status": "ok"}
