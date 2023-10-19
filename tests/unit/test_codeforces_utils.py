f1a8c621778ea880906f814355b5a89df392bc62
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-10-19 12:36:11 +0300

    update codeforces test

--- tests/unit/test_codeforces_utils.py
+++ tests/unit/test_codeforces_utils.py
@@ -1,47 +1,53 @@
-from etr.schemas.contest import ContestSchema
-from etr.schemas.submission import SubmissionSchema
-from etr.schemas.user import UserSchema
-from etr.schemas.team import TeamSchema
-from etr.utils.codeforces_utils import get_contest, get_submission, get_user, get_users, get_problem_with_contest
+from etr.library.codeforces.schemas.contest import CodeforcesContestSchema
+from etr.library.codeforces.schemas.submission import CodeforcesSubmissionSchema
+from etr.library.codeforces.schemas.user import CodeforcesUserSchema
+from etr.library.codeforces.schemas.team import CodeforcesTeamSchema
+from etr.library.codeforces.codeforces_utils import get_contest, get_submission, get_user, get_users, get_problem_with_contest
 
 
 def test_get_contest():
-    contest: ContestSchema | None = get_contest(566)
+    contest: CodeforcesContestSchema | None = get_contest(566)
 
     assert contest is not None, "Contest is None. check get_contest def or network connection."
-    assert contest.name == "VK Cup 2015 - Finals, online mirror", "Incorrect name of the contest."
+    assert contest.contestName == "VK Cup 2015 - Finals, online mirror", "Incorrect name of the contest."
 
 
 def test_get_not_exist_contest():
-    contest: ContestSchema | None = get_contest(-1)
+    contest: CodeforcesContestSchema | None = get_contest(-1)
 
     assert contest is None, "get_contest returns non-existing contest."
 
 
-def test_get_user_submission():  # TODO: rewrite test
-    submissions: list[SubmissionSchema] | None = get_submission(566)
+def test_get_user_submission():
+    submissions: list[CodeforcesSubmissionSchema] | None = get_submission(566)
 
     assert submissions is not None, "Submissions is None. Check get_submission def or network connection."
     assert submissions[-1].id == 12277300, "Submission id is incorrect. Check first submission of contest."
-    assert isinstance(submissions[-1].author, UserSchema), "Author class is incorrect. Check author's parser in get_submission."
+    assert isinstance(
+        submissions[-1].author,
+        CodeforcesUserSchema
+    ), "Author class is incorrect. Check author's parser in get_submission."
     assert submissions[-2].id == 12277309, "Submission id is incorrect. Check second submission of contest."
     assert submissions[-2].problem.name == "Clique in the Divisibility Graph", "Name of contest problem is incorrect. Check problem's parser in get_submission."
 
 
 def test_get_team_submission():
-    submissions: list[SubmissionSchema] | None = get_submission(
+    submissions: list[CodeforcesSubmissionSchema] | None = get_submission(
         566,
         handle="I_love_Hoang_Yen"
     )
 
     assert submissions is not None, "Submission is None. Check get_submission def or network connection."
     assert submissions[4].id == 12874103, "Submission id is incorrect. Check all submission of \"I_love_Hoang_Yen\" on contest."
-    assert isinstance(submissions[15].author, TeamSchema), "Author class is incorrect. Check author's parser in get_submission."
-    assert submissions[15].author.team_name == "Zenith", "team_name is incorrect. Check team parser in get_submission."
+    assert isinstance(
+        submissions[15].author,
+        CodeforcesTeamSchema
+    ), "Author class is incorrect. Check author's parser in get_submission."
+    assert submissions[15].author.teamName == "Zenith", "team_name is incorrect. Check team parser in get_submission."
 
 
 def test_get_not_exist_submission():
-    submissions: list[SubmissionSchema] | None = get_submission(
+    submissions: list[CodeforcesSubmissionSchema] | None = get_submission(
         -1,
         from_=1,
         count=3
@@ -51,10 +57,10 @@ def test_get_not_exist_submission():
 
 
 def test_get_user():
-    user: UserSchema | None = get_user("DmitriyH")
+    user: CodeforcesUserSchema | None = get_user("DmitriyH")
 
     assert user is not None
-    assert user.registration_time_seconds == 1268570311
+    assert user.registrationTimeSeconds == 1268570311
 
 
 def test_get_users():
@@ -62,16 +68,16 @@ def test_get_users():
         "DmitriyH",
         "tourist"
     ]
-    users: list[UserSchema | None] = get_users(handles=handles)
+    users: list[CodeforcesUserSchema | None] = get_users(handles=handles)
 
     assert len(users) == 2, "get_users return the wrong number of users."
     assert users[0] is not None, "get_users return null for an existing user."
     assert users[1] is not None, "get_users return null for an existing user."
-    assert users[1].last_name == "Korotkevich", "last_name is incorrect."
+    assert users[1].lastName == "Korotkevich", "last_name is incorrect."
 
 
 def test_get_not_exist_user():
-    user: UserSchema | None = get_user(
+    user: CodeforcesUserSchema | None = get_user(
         "jasdjaksdjaksdjkasjkdsajkadsjkasjksajkasjdaskjdksaj"
     )
 
