a75a18af9e277f5fa826f7f37cb780a41b2c1278
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-10-21 20:48:06 +0300

    add get_problems_with_contest_id func

--- etr/services/problem.py
+++ etr/services/problem.py
@@ -3,6 +3,7 @@ from etr.utils.factory import create_problem_model
 from etr.library.codeforces.codeforces_utils import get_problem_with_contest
 from etr.db import get_db
 from etr.models.problem import Problem
+from etr.schemas.problem import ProblemSchema
 
 
 
@@ -22,3 +23,21 @@ def add_problems(contest_id: int):
         session.commit()
 
     return problems_schema
+
+
+def get_problems_with_contest_id(contest_id: int) -> list[dict] | None:
+    """ get problems with contest id """
+    with get_db() as session:
+        problems = session.query(Problem).filter(Problem.contest_id == contest_id).all()
+
+        # TODO: Bad code
+        # code should work without this loop
+        for i, _ in enumerate(problems):
+            problems[i].tags = [tag.tag for tag in problems[i].tags]
+
+    problems = [
+        ProblemSchema.model_validate(problem).model_dump()
+        for problem in problems
+    ]
+
+    return problems
