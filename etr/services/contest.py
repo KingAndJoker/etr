f020b876a8118fefa2de0546ec3dc107be260219
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-10-21 15:58:30 +0300

    move problems code to add_problems

--- etr/services/contest.py
+++ etr/services/contest.py
@@ -2,12 +2,10 @@
 from etr.db import get_db
 from etr.schemas.contest import ContestSchema
 from etr.models.contest import Contest
-from etr.models.problem import Problem
-from etr.library.codeforces.codeforces_utils import get_contest, get_problem_with_contest
-from etr.utils.codeforces.convert import (
-    convert_codeforces_contest_schema,
-    convert_codeforces_problems_schema
-)
+from etr.library.codeforces.codeforces_utils import get_contest
+from etr.utils.codeforces.convert import convert_codeforces_contest_schema
+from etr.services.problem import add_problems
+from etr.utils.factory import create_contest_model
 
 
 def add_contest(contest_id: int) -> ContestSchema:
@@ -16,20 +14,13 @@ def add_contest(contest_id: int) -> ContestSchema:
     contest_schema = convert_codeforces_contest_schema(
         get_contest(contest_id=contest_id)
     )
-    problems_schema = convert_codeforces_problems_schema(
-        get_problem_with_contest(contest_id)
-    )
 
     with get_db() as session:
-        contest = Contest(**contest_schema.model_dump())
+        contest = create_contest_model(**contest_schema.model_dump()) # Contest(**contest_schema.model_dump())
         session.add(contest)
 
-        problems = [
-            Problem(**problem_schema.model_dump())
-            for problem_schema in problems_schema
-        ]
-        session.add_all(problems)
-
         session.commit()
+
+    problems_schema = add_problems(contest_id)
 
     return contest_schema
