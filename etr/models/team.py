f1bbb441592f45596e47df506f2b947759bd1b66
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-10-25 21:08:41 +0300

    add TODO rename teamName -> team_name

--- etr/models/team.py
+++ etr/models/team.py
@@ -29,6 +29,7 @@ class Team(Base):
     __tablename__ = "teams"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    # TODO: rename to team_name
     teamName: Mapped[str] = mapped_column(type_=String(255))
 
     users: Mapped[list[User]] = relationship(secondary=teams_users)
