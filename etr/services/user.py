33144b1b24e67d80d69e1dfd05f9ede82a0de793
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-10-26 13:44:20 +0300

    refactor user services

--- etr/services/user.py
+++ etr/services/user.py
@@ -2,13 +2,39 @@ from etr.db import get_db
 from etr.models.user import User
 from etr.schemas.user import UserSchema
 
-def get_users() -> list[dict]:
-    with get_db() as session:
-        users = session.query(User).filter(User.watch).all()
 
+def _get_user_db_with_handle(handle: str, watch: bool = True) -> User | None:
+    with get_db() as session:
+        user = session.query(User).filter_by(
+            handle=handle,
+            watch=watch
+        ).one_or_none()
+
+    return user
+
+
+def _get_all_users(watch: bool = True) -> list[User]:
+    with get_db() as session:
+        users = session.query(User).all()
+
+    return users
+
+
+def _get_users_db_with_handle(handles: list[str], watch: bool = True) -> list[User]:
     users = [
-        UserSchema.model_validate(user).model_dump()
-        for user in users
+        user := _get_user_db_with_handle(handle, watch)
+        for handle in handles if user
     ]
 
     return users
+
+
+def get_user(handle: str, watch: bool = True) -> UserSchema | None:
+    user_db = _get_user_db_with_handle(handle, watch)
+
+    if user_db is None:
+        return None
+
+    user = UserSchema.model_validate(user_db)
+
+    return user
