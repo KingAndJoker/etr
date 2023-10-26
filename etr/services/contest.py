626bd0da81ae33f52a8d51a0db7a4b5637eead6d
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-10-26 13:00:56 +0300

    add update_contest to services

--- etr/services/contest.py
+++ etr/services/contest.py
@@ -25,3 +25,37 @@ def add_contest(contest_id: int) -> ContestSchema:
     contest_schema.problems = problems_schema
 
     return contest_schema
+
+
+def get_contest_with_db(contest_id: int) -> Contest:
+    with get_db() as session:
+        contest_db = session.query(Contest).filter_by(
+            id=contest_id
+        ).one_or_none()
+    
+    return contest_db
+
+
+def _update_contest_model_fields(contest: Contest, **kwargs) -> Contest:
+    for field, value in kwargs.items():
+        setattr(contest, field, value)
+    
+    return contest
+
+
+def update_contest(contest_id: int) -> ContestSchema | None:
+    contest_schema = convert_codeforces_contest_schema(
+        get_contest(contest_id)
+    )
+
+    if contest_schema is None:
+        return None
+    
+    contest_db = get_contest_with_db(contest_id)
+
+    if contest_db is None:
+        contest_db = create_contest_model(**contest_schema.model_dump())
+    else:
+        contest_db = _update_contest_model_fields(contest_db, **contest_schema.model_dump())
+
+    return contest_schema
