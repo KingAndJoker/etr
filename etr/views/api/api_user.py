d9525ce5f98622b0e644363e726461deb9e2fb07
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-10-27 11:30:05 +0300

    fix bug: return schema (Object of type is not JSON serializable)

--- etr/views/api/api_user.py
+++ etr/views/api/api_user.py
@@ -9,6 +9,14 @@ bp = Blueprint("api_user", __name__)
 
 @bp.get("/user/")
 def get_all_users():
-    users = get_users()
+    users_schema = get_users()
+
+    if users_schema is None:
+        return {"status": "error"}
+    
+    users = [
+        user_schema.model_dump()
+        for user_schema in users_schema
+    ]
 
     return users
