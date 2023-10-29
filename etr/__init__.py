0efd9a2709cc67a87d15734c50e75a76327fca30
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-10-29 21:22:16 +0300

    refactor engine create

--- etr/__init__.py
+++ etr/__init__.py
@@ -4,32 +4,35 @@ import os
 from flask import Flask
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
+from sqlalchemy import Engine
 
 from .models.base import Base
 
 
-def create_app() -> Flask:
+load_dotenv()
+
+URL_PREFIX = os.getenv("URL_PREFIX", "")
+DEFAULT_DATABASE_URL = "sqlite:///storage/users.db"
+DATABASE_URL = os.getenv("URL_DATABASE", DEFAULT_DATABASE_URL)
+ECHO = os.getenv("DATABASE_ECHO", "True") in ("true", "True")
+
+
+def create_app(engine: Engine | None = None) -> Flask:
     """Create and configure an instance of the Flask application."""
 
-    URL_PREFIX = os.getenv("URL_PREFIX", "")
+    app = Flask(__name__, instance_relative_config=True,
+                static_url_path=f"{URL_PREFIX}/static")
 
-    app = Flask(__name__, instance_relative_config=True, static_url_path=f"{URL_PREFIX}/static")
-    
-    load_dotenv()
     app.config.from_mapping(
-        # a default secret that should be overridden by instance config
         SECRET_KEY="dev",
     )
-    # app.config.from_file("pyproject.toml")
 
-    # register the database commands
-    from etr import db
+    if engine is None:
+        from etr import db
 
-    # Base.metadata.create_all(app.config["DATABASE_ENGINE"])
-    db.init_db(app)
-    app.static_url_path
+        engine = db.init_db(DATABASE_URL, ECHO)
+    app.config["engine"] = engine
 
-    # apply the blueprints to the app
     from etr.views import user
     from etr.views import index
     from etr.views import contest
@@ -41,15 +44,11 @@ def create_app() -> Flask:
     app.register_blueprint(index.bp, url_prefix=URL_PREFIX)
     app.register_blueprint(user.bp, url_prefix=URL_PREFIX + "/user")
     app.register_blueprint(contest.bp, url_prefix=URL_PREFIX + "/contest")
-    app.register_blueprint(contest_status.bp, url_prefix=URL_PREFIX + "/status")
+    app.register_blueprint(
+        contest_status.bp, url_prefix=URL_PREFIX + "/status")
     app.register_blueprint(api_problem.bp, url_prefix=URL_PREFIX + "/api")
     app.register_blueprint(api_user.bp, url_prefix=URL_PREFIX + "/api")
     app.register_blueprint(api_submission.bp, url_prefix=URL_PREFIX + "/api")
     app.register_blueprint(rpc.bp, url_prefix=URL_PREFIX + "/rpc")
-
-    # make url_for('index') == url_for('blog.index')
-    # in another app, you might define a separate main index here with
-    # app.route, while giving the blog blueprint a url_prefix, but for
-    # the tutorial the blog will be the main index
 
     return app
