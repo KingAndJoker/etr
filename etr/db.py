293ce62afc1cf39e687f76e6216ce33501394477
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-10-29 21:03:24 +0300

    refactor db.py

--- etr/db.py
+++ etr/db.py
@@ -12,21 +12,15 @@ from etr.models.problem import Problem
 from etr.models.team import Team, teams_users
 
 
-DEFAULT_DATABASE_URL = "sqlite:///storage/users.db"
-DATABASE_URL = os.getenv("URL_DATABASE", DEFAULT_DATABASE_URL)
-ECHO = os.getenv("DATABASE_ECHO", "True") in ("true", "True")
 engine = None
 
 
-def init_db(app: Flask) -> None:
-    engine = create_engine(DATABASE_URL, echo=ECHO)
+def init_db(database_url: str, echo: bool = False) -> None:
+    global engine
+    engine = create_engine(database_url, echo=echo)
     Base.metadata.create_all(engine)
+    return engine
 
 
 def get_db() -> Session:
-    global engine
-    if engine:
-        return Session(engine)
-    else:
-        engine = create_engine(DATABASE_URL, echo=ECHO)
-        return Session(engine)
+    return Session(engine)
