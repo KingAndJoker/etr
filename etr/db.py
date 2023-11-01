b1b1ad4e9e1fa2c431d6f31256d5e8f5bb852a09
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-11-01 21:37:44 +0300

    update return type

--- etr/db.py
+++ etr/db.py
@@ -1,7 +1,7 @@
 """db utils"""
 import os
 from flask import Flask
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import Session
 
 from etr.models.base import Base
@@ -15,7 +15,7 @@ from etr.models.team import Team, teams_users
 engine = None
 
 
-def init_db(database_url: str, echo: bool = False) -> None:
+def init_db(database_url: str, echo: bool = False) -> Engine:
     global engine
     engine = create_engine(database_url, echo=echo)
     Base.metadata.create_all(engine)
