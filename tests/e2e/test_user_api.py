4f3454a672d9f719246e016ce1f7740c83eed67d
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-11-04 14:16:05 +0300

    move test up

--- tests/e2e/test_user_api.py
+++ tests/e2e/test_user_api.py
@@ -9,6 +9,41 @@ load_dotenv()
 URL_PREFIX = os.getenv("URL_PREFIX", "")
 
 
+def test_patch_user(client: FlaskClient):
+    response = client.patch(f"{URL_PREFIX}/api/user/1", json={
+        "first_name": "Evgeniy",
+        "last_name": "Onegin",
+        "city": "Moscow",
+        "country": "Russia",
+        "organization": "GSU",
+        "rank": "master",
+        "max_rank": "master",
+        "email": "test@mail.com"
+    })
+
+    assert response.status_code == 200
+    assert response.json["status"] == "ok"
+    assert response.json["result"] is not None
+    assert response.json["result"]["city"] == "Moscow"
+    assert response.json["result"]["contribution"] is None
+    assert response.json["result"]["country"] == "Russia"
+    assert response.json["result"]["email"] == "test@mail.com"
+    assert response.json["result"]["first_name"] == "Evgeniy"
+    assert response.json["result"]["last_name"] == "Onegin"
+    assert response.json["result"]["organization"] == "GSU"
+    assert response.json["result"]["rank"] == "master"
+    assert response.json["result"]["max_rank"] == "master"
+
+    response = client.get(f"{URL_PREFIX}/api/user/?handles=Senior")
+    assert response.status_code == 200
+    assert response.json["status"] == "ok"
+    assert response.json["users"] is not None
+    assert len(response.json["users"]) == 1
+    assert response.json["users"][0]["city"] == "Moscow"
+    assert response.json["users"][0]["country"] == "Russia"
+    assert response.json["users"][0]["email"] == "test@mail.com"
+
+
 def test_get_users(client: FlaskClient):
     response = client.get(f"{URL_PREFIX}/api/user/")
     assert response.status_code == 200
@@ -277,13 +312,11 @@ def test_post_user(client: FlaskClient):
     assert response.json["user"] is not None
     assert response.json["user"]["avatar"] == "https://userpic.codeforces.org/no-avatar.jpg"
     assert response.json["user"]["city"] is None
-    assert response.json["user"]["contribution"] == 0
     assert response.json["user"]["country"] is None
     assert response.json["user"]["email"] is None
     assert response.json["user"]["first_name"] is None
-    assert response.json["user"]["friend_of_count"] == 371
     assert response.json["user"]["handle"] == "inaFSTream"
-    assert response.json["user"]["id"] is None
+    assert response.json["user"]["id"] == 6
     assert response.json["user"]["last_name"] is None
     assert response.json["user"]["max_rank"] == "legendary grandmaster"
     assert response.json["user"]["max_rating"] == 3478
@@ -294,38 +327,3 @@ def test_post_user(client: FlaskClient):
     assert response.json["user"]["registration_time_seconds"] == 1515819239
     assert response.json["user"]["title_photo"] == "https://userpic.codeforces.org/no-title.jpg"
     assert response.json["user"]["vk_id"] is None
-
-
-def test_patch_user(client: FlaskClient):
-    response = client.patch(f"{URL_PREFIX}/api/user/1", json={
-        "first_name": "Evgeniy",
-        "last_name": "Onegin",
-        "city": "Moscow",
-        "country": "Russia",
-        "organization": "GSU",
-        "rank": "master",
-        "max_rank": "master",
-        "email": "test@mail.com"
-    })
-
-    assert response.status_code == 200
-    assert response.json["status"] == "ok"
-    assert response.json["result"] is not None
-    assert response.json["result"]["city"] == "Moscow"
-    assert response.json["result"]["contribution"] is None
-    assert response.json["result"]["country"] == "Russia"
-    assert response.json["result"]["email"] == "test@mail.com"
-    assert response.json["result"]["first_name"] == "Evgeniy"
-    assert response.json["result"]["last_name"] == "Onegin"
-    assert response.json["result"]["organization"] == "GSU"
-    assert response.json["result"]["rank"] == "master"
-    assert response.json["result"]["max_rank"] == "master"
-
-    response = client.get(f"{URL_PREFIX}/api/user/?handles=Senior")
-    assert response.status_code == 200
-    assert response.json["status"] == "ok"
-    assert response.json["users"] is not None
-    assert len(response.json["users"]) == 1
-    assert response.json["users"][0]["city"] == "Moscow"
-    assert response.json["users"][0]["country"] == "Russia"
-    assert response.json["users"][0]["email"] == "test@mail.com"
