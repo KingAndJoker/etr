579679213b3a7d50d7c720e592166c5c6b776522
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-11-30 21:06:58 +0300

    add new user schemas

--- etr/schemas/user.py
+++ etr/schemas/user.py
@@ -37,3 +37,29 @@ class UserSchema(BaseModel):
     avatar: str | None = None
     title_photo: str | None = Field(default=None, alias="titlePhoto")
     grade: int | None = None
+
+
+class UserRequestAddCodeforcesSchema(BaseModel):
+    handle: str
+
+
+class UserPatch(BaseModel):
+    email: str | None = None
+    vk_id: str | None = None
+    open_id: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
+    country: str | None = None
+    city: str | None = None
+    organization: str | None = None
+    contribution: int | None = None
+    rank: str | None = None
+    rating: int | None = None
+    max_rank: str | None = None
+    max_rating: int | None = None
+    last_online_time_seconds: int | None = None
+    registration_time_seconds: int | None = None
+    friend_of_count: int | None = None
+    avatar: str | None = None
+    title_photo: str | None = None
+    grade: int | None = None
