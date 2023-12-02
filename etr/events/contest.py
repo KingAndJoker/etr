768b084a3c97c186453d1b32bf0e1f1d2d79033a
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-12-02 20:06:20 +0300

    add events

---
+++ etr/events/contest.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+from . import Event
+from etr.schemas.contest import ContestUrl
+
+
+@dataclass
+class UpdateContest(Event):
+    pass
+
+
+@dataclass
+class ParseContestBeforeUpdate(Event):
+    contest_url: ContestUrl
