bd90e56a8a1f8a1613d0a0d77dafbf7bb85ecd73
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-12-02 22:44:32 +0300

    add ResultOfHandleEvent class

--- etr/events/__init__.py
+++ etr/events/__init__.py
@@ -1,2 +1,16 @@
 class Event:
     pass
+
+
+class ResultOfHandleEvent:
+    events: list[Event]
+    results: list
+
+    def __init__(self, events=None, results=None) -> None:
+        if events is None:
+            events = []
+        if results is None:
+            results = []
+
+        self.events = events
+        self.results = results
