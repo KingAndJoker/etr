48df4cfea99eebcd2623319e3c0760978fc1b57b
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-12-02 23:30:57 +0300

    add AddContest handler, rewrite set of results, queue

--- etr/handlers/__init__.py
+++ etr/handlers/__init__.py
@@ -1,7 +1,7 @@
 from typing import Callable
 
 from etr.events import Event, ResultOfHandleEvent
-from etr.events.contest import UpdateContest
+from etr.events.contest import ParseContestBeforeUpdate
 from etr.events.contest import ParseCodeforces
 from etr.events.contest import ParseCodeforcesContest
 from etr.events.contest import ParseCodeforcesGym
@@ -10,14 +10,15 @@ from etr.handlers.contest import handle_contest_type
 from etr.handlers.contest import handle_codeforces_type_contest
 from etr.handlers.contest import handle_codeforces_contest
 from etr.handlers.contest import handle_codeforces_gym
+from etr.handlers.contest import handle_add_contest
 
 
 HANDLERS: dict[Event, list[Callable[[Event], ResultOfHandleEvent]]] = {
-    UpdateContest: [handle_contest_type],
+    ParseContestBeforeUpdate: [handle_contest_type],
     ParseCodeforces: [handle_codeforces_type_contest],
     ParseCodeforcesContest: [handle_codeforces_contest],
     ParseCodeforcesGym: [handle_codeforces_gym],
-    AddContest: []
+    AddContest: [handle_add_contest],
 }
 
 
@@ -28,6 +29,6 @@ def handle(event: Event) -> list:
         event = queue.pop(0)
         for handler in HANDLERS[type(event)]:
             result_of_work = handler(event)
-            results.append(result_of_work.results)
+            results.extend(result_of_work.results)
             queue.extend(result_of_work.events)
     return results
