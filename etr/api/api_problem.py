48477186b587d921100b0a6d6b17b140cd6303ce
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2023-12-10 14:46:41 +0300

    update responses api problem

--- etr/api/api_problem.py
+++ etr/api/api_problem.py
@@ -1,13 +1,31 @@
 """API problem"""
 from fastapi import APIRouter
 
-from etr.services.problem import get_problems_with_contest_id
+from etr.services.problem import get_problems_with_contest_id, get_problems
 
 
-router = APIRouter(
-    prefix="/problem",
-    tags=["problems"]
-)
+router = APIRouter(prefix="/problem", tags=["problems"])
+
+
+@router.get("/")
+def api_get_problems(
+    id_: int | None = None,
+    contest_id: int | None = None,
+    problemset_name: str | None = None,
+    index: str | None = None,
+):
+    params = {
+        "id": id_,
+        "contest_id": contest_id,
+        "problemset_name": problemset_name,
+        "index": index,
+    }
+    params = {key: value for key, value in params.items() if value is not None}
+    problems = get_problems(**params)
+    return {
+        "status": "ok",
+        "problems": [problem.model_dump() for problem in problems]
+    }
 
 
 @router.get("/{contest_id}")
@@ -15,4 +33,4 @@ def get_problem_with_contest(contest_id: int):
     """Get problem with contest"""
 
     problems_schema = get_problems_with_contest_id(contest_id)
-    return problems_schema
+    return {"status": "ok", "problems": problems_schema}
