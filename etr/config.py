edb2993fea94d8ab673e6e482b7a5d39708f2f23
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2024-02-28 13:56:25 +0300

    add DELAY_SYNC_SUBMISSIONS_USERS, DEBUG

--- etr/config.py
+++ etr/config.py
@@ -10,3 +10,5 @@ DEFAULT_DATABASE_URL = "sqlite:///:memory:"
 DATABASE_URL = os.getenv("URL_DATABASE", DEFAULT_DATABASE_URL)
 SQLALCHEMY_ECHO = os.getenv("DATABASE_ECHO", "true").lower() == "true"
 SQL_PASSWORD = os.getenv("SQL_PASSWORD", "")
+DELAY_SYNC_SUBMISSIONS_USERS = 3600
+DEBUG = os.getenv("DEBUG", "true").lower() == "true"
