b56fa6cf58babe0fc8b3d3a91a7bbaa5f66aaef9
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2024-02-28 13:42:28 +0300

    add button

--- etr/static/js/submission_api.js
+++ etr/static/js/submission_api.js
@@ -7,3 +7,13 @@ const send_update_users_submissions = async (handle) => {
         alert("произошла ошибка, попробуйте повторить запрос позже")
     }
 }
+
+const send_request_update_all_submissions_for_all_users = async () => {
+    let response = await fetch(`/etr/rpc/submissions/users`)
+    if (response.ok) {
+        alert("пользователи обновляются")
+    }
+    else {
+        alert("произошла ошибка, попробуйте повторить запрос позже")
+    }
+}
