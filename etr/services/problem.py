f9beed2ffddfc68162b6bfc7038d712dc4e44794
KingAndJoker <60524574+KingAndJoker@users.noreply.github.com>
2024-04-10 12:08:02 +0300

    add update_solved_count

--- etr/services/problem.py
+++ etr/services/problem.py
@@ -1,8 +1,12 @@
+from etr import db
 from etr.crud.problem import *
 from etr.crud.tag import get_tag_by_name
 from etr.crud.tag import create_tag
 from etr.crud.contest import get_contests
-from etr.models.problem import TagOrm
+from etr.models.problem import ProblemOrm, TagOrm
+from etr.library.codeforces import problemset
+from etr.utils.codeforces.convert import convert_codeforces_problems_schema
+from etr.utils.codeforces.convert import convert_codeforces_problems_statistics
 
 
 def add_missing_problem_with_contest(contest_id: int) -> list[ProblemSchema]:
@@ -27,7 +31,9 @@ def add_missing_problem_with_contest(contest_id: int) -> list[ProblemSchema]:
 
 def update_tag_by_problem(problem_id: int, tag: str):
     with db.SessionLocal() as session:
-        problem_orm = session.query(ProblemOrm).filter_by(id=problem_id).one_or_none()
+        problem_orm = session.query(ProblemOrm).filter_by(
+            id=problem_id
+        ).one_or_none()
         tag_orm = session.query(TagOrm).filter_by(tag=tag).one_or_none()
         if problem_orm is None or tag_orm is None:
             return
@@ -68,3 +74,22 @@ def update_tags_in_problem_of_contest(contest_id: int) -> list[ProblemSchema]:
         problems.append(problem)
 
     return problems
+
+
+def update_solved_count():
+    _, problems_statistics_codeforces = problemset.problems()
+    problems_statistics = convert_codeforces_problems_statistics(
+        problems_statistics_codeforces
+    )
+
+    with db.SessionLocal() as session:  # неправильно, но зато быстро
+        for problem_statistics in problems_statistics:
+            if problem_statistics.contest_id is not None:
+                problem_orm = session.query(ProblemOrm).filter_by(
+                    contest_id=problem_statistics.contest_id,
+                    index=problem_statistics.index
+                ).one_or_none()
+                if problem_orm is not None:
+                    problem_orm.solved_count = problem_statistics.solved_count
+                    session.add(problem_orm)
+        session.commit()
